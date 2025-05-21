f8b758cd4a34e2694c4616663c1e29ab51f6b950
genki-kurokawa <hongi.ftisland@gmail.com>
2025-05-22 01:39:43 +0900

    up

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+const addBtn = document.getElementById('btn');
+
+const parentList = document.getElementById('text');
+
+
+addBtn.addEventListener('click', () => {
+    
+   text.textContent = 'ボタンをクリックしました';
+
+});
